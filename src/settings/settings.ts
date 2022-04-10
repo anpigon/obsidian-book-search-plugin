ec9bd5c7c1520b4b03474695e32429ec976a4289
anpigon <markan82@gmail.com>
2022-04-10 18:35:34 +0900

    add: frontmatter in settings

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -5,10 +5,12 @@ import { FolderSuggest } from './suggesters/FolderSuggester';
 
 export interface BookSearchPluginSettings {
   folder: string;
+  frontmatter: string;
 }
 
 export const DEFAULT_SETTINGS: BookSearchPluginSettings = {
   folder: '',
+  frontmatter: '',
 };
 
 export class BookSearchSettingTab extends PluginSettingTab {
@@ -37,6 +39,18 @@ export class BookSearchSettingTab extends PluginSettingTab {
             this.plugin.settings.folder = new_folder;
             this.plugin.saveSettings();
           });
+      });
+
+    new Setting(containerEl)
+      .setName('Insert frontmatter')
+      .setDesc('Text to insert into the YAML frontmatter')
+      .addTextArea(textArea => {
+        const prevValue = this.plugin.settings.frontmatter;
+        textArea.setValue(prevValue).onChange(async value => {
+          const newValue = value;
+          this.plugin.settings.frontmatter = newValue;
+          await this.plugin.saveSettings();
+        });
       });
   }
 }
