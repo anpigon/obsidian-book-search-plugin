541370d77762d777d34bf5e69c73554bd0a5de41
anpigon <markan82@gmail.com>
2022-07-24 22:35:46 +0900

    test: function replaceIllegalFileNameCharactersInString in utils.ts

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,7 +1,9 @@
 import * as utils from './utils';
 
+jest.mock('@settings/settings', () => jest.fn());
+
 describe('util.js', () => {
   it('replaceIllegalFileNameCharactersInString', () => {
-    expect(1+1).toBe(2)
-  })
-})
+    expect(utils.replaceIllegalFileNameCharactersInString('재레드 다이아몬드의 <대변동 : 위기, 선택, 변화>')).toBe('재레드 다이아몬드의 대변동 위기 선택 변화');
+  });
+});
