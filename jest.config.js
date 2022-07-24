8b4906cc3dbeab1b284d6d8e96292b04a712b1c0
anpigon <markan82@gmail.com>
2022-07-24 22:03:13 +0900

    chore: path aliases in jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,19 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: [
     "src/**/*.{js,ts}",
-    "!**/node_modules/**",
-    "!**/vendor/**"
-  ]
+    "!**/node_modules/**"
+  ],
+  roots: [ '<rootDir>/src' ],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    "@settings/(.*)": '<rootDir>/src/settings/$1',
+    "@models/(.*)": '<rootDir>/src/models/$1',
+    "@editor/(.*)": '<rootDir>/src/editor/$1',
+    "@utils/(.*)": '<rootDir>/src/utils/$1',
+    "@apis/(.*)": '<rootDir>/src/apis/$1',
+    "@src/(.*)": '<rootDir>/src/$1'
+  }
 };
