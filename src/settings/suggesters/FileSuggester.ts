f4d0c23ef2833ef588b019012ac0567e1a3bcece
anpigon <markan82@gmail.com>
2022-08-14 12:13:56 +0900

    addL FileSuggester

---
+++ src/settings/suggesters/FileSuggester.ts
@@ -0,0 +1,34 @@
+// Credits go to Liam's Periodic Notes Plugin: https://github.com/liamcain/obsidian-periodic-notes
+
+import { TAbstractFile, TFile, TFolder } from 'obsidian';
+import { TextInputSuggest } from './suggest';
+
+export class FileSuggest extends TextInputSuggest<TFile> {
+  getSuggestions(inputStr: string): TFile[] {
+    const abstractFiles = this.app.vault.getAllLoadedFiles();
+    const files: TFile[] = [];
+    const lowerCaseInputStr = inputStr.toLowerCase();
+
+    abstractFiles.forEach((file: TAbstractFile) => {
+      if (
+        file instanceof TFile &&
+        file.extension === "md" &&
+        file.path.toLowerCase().contains(lowerCaseInputStr)
+      ) {
+        files.push(file);
+      }
+    });
+
+    return files;
+  }
+
+  renderSuggestion(file: TFile, el: HTMLElement): void {
+    el.setText(file.path);
+  }
+
+  selectSuggestion(file: TFile): void {
+    this.inputEl.value = file.path;
+    this.inputEl.trigger("input");
+    this.close();
+  }
+}
