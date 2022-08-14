3643e73137ce8dda9f95f556502025d652e094a3
anpigon <markan82@gmail.com>
2022-08-14 13:05:37 +0900

    add: function applyTemplateTransformations in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,4 @@
+import type { Moment } from "moment";
 import { Book, FrontMatter } from '@models/book.model';
 import { DefaultFrontmatterKeyType } from '@settings/settings';
 import { App, normalizePath, Notice } from 'obsidian';
@@ -159,4 +160,29 @@ export async function getTemplateContents(app: App, templatePath: string | undef
     new Notice('Failed to read the daily note template');
     return '';
   }
+}
+
+export function applyTemplateTransformations(
+  rawTemplateContents: string,
+): string {
+ return rawTemplateContents
+    .replace(
+      /{{\s*(date|time)\s*(([+-]\d+)([yqmwdhs]))?\s*(:.+?)?}}/gi,
+      (_, _timeOrDate, calc, timeDelta, unit, momentFormat) => {
+        const now = window.moment();
+        const currentDate = window.moment().clone().set({
+          hour: now.get('hour'),
+          minute: now.get('minute'),
+          second: now.get('second'),
+        });
+        if (calc) {
+          currentDate.add(parseInt(timeDelta, 10), unit);
+        }
+
+        if (momentFormat) {
+          return currentDate.format(momentFormat.substring(1).trim());
+        }
+        return currentDate.format('YYYY-MM-DD');
+      },
+    );
 }
