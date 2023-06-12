415cf0fa5691314d66072d8bab81326faa7b6e77
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-06-12 10:05:55 -0400

    adds api call to get game details

--- src/apis/rawg_games_api.ts
+++ src/apis/rawg_games_api.ts
@@ -1,10 +1,10 @@
-import { RAWGResponse, Game } from '@models/game.model';
+import { RAWGListResponse, GameFromSearch, Game } from '@models/game.model';
 import { requestUrl } from 'obsidian';
 
 export class RAWGAPI {
   constructor(private readonly key: string) {}
 
-  async getByQuery(query: string): Promise<Game[]> {
+  async getByQuery(query: string): Promise<GameFromSearch[]> {
     try {
       const apiURL = new URL('https://api.rawg.io/api/games');
       apiURL.searchParams.append('key', this.key);
@@ -15,13 +15,36 @@ export class RAWGAPI {
         method: 'GET',
       });
 
-      const searchResults = res.json as RAWGResponse<Game>;
+      const searchResults = res.json as RAWGListResponse<GameFromSearch>;
 
       if (!searchResults?.count) {
         return [];
       }
 
       return searchResults.results;
+    } catch (error) {
+      console.warn(error);
+      throw error;
+    }
+  }
+
+  async getBySlug(slug: string): Promise<Game> {
+    try {
+      const apiURL = new URL('https://api.rawg.io/api/games/' + slug);
+      apiURL.searchParams.append('key', this.key);
+
+      const res = await requestUrl({
+        url: apiURL.href,
+        method: 'GET',
+      });
+
+      const searchResults = res.json as Game;
+
+      if (searchResults == null) {
+        throw new Error('game not found or error parsing data');
+      }
+
+      return searchResults;
     } catch (error) {
       console.warn(error);
       throw error;
