81adec4506aa214eba3787e9e5c9ecf1db629f03
anpigon <markan82@gmail.com>
2023-12-10 12:14:14 +0900

    fix(apis): remove unnecessary optional chaining operator in safeStorage assignment
    fix(settings): remove unnecessary optional chaining operator in safeStorage assignment

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -10,7 +10,7 @@ import { SettingServiceProviderModal } from '@views/setting_service_provider_mod
 
 const docUrl = 'https://github.com/anpigon/obsidian-book-search-plugin';
 
-const safeStorage = (window as any).electron?.remote.safeStorage;
+const safeStorage = window.electron?.remote.safeStorage;
 
 export enum DefaultFrontmatterKeyType {
   snakeCase = 'Snake Case',
