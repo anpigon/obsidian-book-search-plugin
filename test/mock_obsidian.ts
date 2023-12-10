d9dc37ff67a40ee7d346335849afcaa4e3e57311
anpigon <markan82@gmail.com>
2023-12-10 11:56:37 +0900

    fix(mock_obsidian.ts): add type annotation to the return value of requestUrl function to improve type safety and clarity

--- test/mock_obsidian.ts
+++ test/mock_obsidian.ts
@@ -1,5 +1,5 @@
 import * as obsidian from 'obsidian';
 
 export const requestUrl: typeof obsidian.requestUrl = req => {
-  return fetch(req as never).then(res => res.json());
+  return fetch(req as never).then(res => res.json()) as obsidian.RequestUrlResponsePromise;
 };
