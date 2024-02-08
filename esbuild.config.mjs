b752a2b99337dbf92a4aa5902d03eccb89419a6f
anpigon <markan82@gmail.com>
2024-02-08 18:11:00 +0900

    Refactor esbuild configuration

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -10,46 +10,39 @@ if you want to view the source, please visit the github repository of this plugi
 
 const prod = process.argv[2] === 'production';
 
-esbuild
-  .build({
-    banner: {
-      js: banner,
-    },
-    minify: prod ? true : false,
-    entryPoints: ['src/main.ts'],
-    bundle: true,
-    external: [
-      'obsidian',
-      'electron',
-      '@codemirror/autocomplete',
-      '@codemirror/closebrackets',
-      '@codemirror/collab',
-      '@codemirror/commands',
-      '@codemirror/comment',
-      '@codemirror/fold',
-      '@codemirror/gutter',
-      '@codemirror/highlight',
-      '@codemirror/history',
-      '@codemirror/language',
-      '@codemirror/lint',
-      '@codemirror/matchbrackets',
-      '@codemirror/panel',
-      '@codemirror/rangeset',
-      '@codemirror/rectangular-selection',
-      '@codemirror/search',
-      '@codemirror/state',
-      '@codemirror/stream-parser',
-      '@codemirror/text',
-      '@codemirror/tooltip',
-      '@codemirror/view',
-      ...builtins,
-    ],
-    format: 'cjs',
-    watch: !prod,
-    target: 'es2016',
-    logLevel: 'info',
-    sourcemap: prod ? false : 'inline',
-    treeShaking: true,
-    outfile: 'main.js',
-  })
-  .catch(() => process.exit(1));
+const context = await esbuild.context({
+  banner: {
+    js: banner,
+  },
+  entryPoints: ['src/main.ts'],
+  bundle: true,
+  external: [
+    'obsidian',
+    'electron',
+    '@codemirror/autocomplete',
+    '@codemirror/collab',
+    '@codemirror/commands',
+    '@codemirror/language',
+    '@codemirror/lint',
+    '@codemirror/search',
+    '@codemirror/state',
+    '@codemirror/view',
+    '@lezer/common',
+    '@lezer/highlight',
+    '@lezer/lr',
+    ...builtins,
+  ],
+  format: 'cjs',
+  target: 'es2018',
+  logLevel: 'info',
+  sourcemap: prod ? false : 'inline',
+  treeShaking: true,
+  outfile: 'main.js',
+});
+
+if (prod) {
+  await context.rebuild();
+  process.exit(0);
+} else {
+  await context.watch();
+}
